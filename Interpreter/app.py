0f8b99db3e9e2a44ec00595246ff2939e1962bbc
JosePirir <josepirirromero@gmail.com>
2025-06-18 21:11:42 -0600

    Entrega Proyecto 1 COMPI

--- Interpreter/app.py
+++ Interpreter/app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 def inicio():
     resultado = ''
     if request.method == 'POST':
-        # texto = request.form['codigo'].lower()
-        texto = request.form['codigo']
+        #texto = request.form['codigo'].lower()
+        texto = request.form['codigo'].lower()
         resultado = analizar_texto(texto)
     return render_template('index.html', resultado=resultado)
 
